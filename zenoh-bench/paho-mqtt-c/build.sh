d9df1b2c744180de84c2f99de6414798aa8bf275
aeon <jerry73204@gmail.com>
2023-03-05 07:58:34 +0800

    Tidy build files

--- zenoh-bench/paho-mqtt-c/build.sh
+++ zenoh-bench/paho-mqtt-c/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 cmake -B build -S paho.mqtt.c-1.3.10 \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DCMAKE_INSTALL_PREFIX=/usr \
       -DPAHO_WITH_SSL=TRUE \
       -DPAHO_BUILD_SAMPLES=TRUE
