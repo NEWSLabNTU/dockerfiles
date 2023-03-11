9d2944744dabc0e9f628e5306258118e77404649
aeon <jerry73204@gmail.com>
2023-03-11 18:19:07 +0800

    Install Kafka and rdkafka in zenoh-bench image

---
+++ zenoh-bench/kafka/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -e
+mv kafka_2.13-3.2.1 /opt/kafka
+chown -R root:root /opt/kafka
---
+++ zenoh-bench/kafka/prepare.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -e
+wget 'https://archive.apache.org/dist/kafka/3.2.1/kafka_2.13-3.2.1.tgz'
+tar -xf kafka_2.13-3.2.1.tgz
