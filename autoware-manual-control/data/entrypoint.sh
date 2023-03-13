e1afa43fe48357c0c5434e9472ebcdc42e567955
aeon <jerry73204@gmail.com>
2023-03-13 10:30:53 +0800

    Fix a bug that autoware_manual_control does not relay topics

--- autoware-manual-control/data/entrypoint.sh
+++ autoware-manual-control/data/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 source /root/autoware_manual_control_ws/install/setup.bash
+ros2 launch /root/autoware_manual_control_ws/relay.launch.xml &
 ros2 run autoware_manual_control keyboard_control $@
